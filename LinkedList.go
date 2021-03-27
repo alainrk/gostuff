a180a872b0929b7b9024041911a2aec5c87d4eb0
Alain Di Chiappari <alain.devcs@gmail.com>
2021-03-27 13:54:43 +0100

    pushback pushhead and stuff

--- LinkedList.go
+++ LinkedList.go
@@ -6,12 +6,15 @@ import (
     "strconv"
 )
 
-// type LinkedLister interface {
-//     PushHead()
-//     Remove()
-//     Length()
-//     Visit()
-// }
+type LinkedLister interface {
+    PushHead()
+    PushBack()
+    Head()
+    Tail()
+    Remove()
+    Length()
+    Visit()
+}
 
 type Node struct {
     key string
@@ -29,11 +32,36 @@ type LinkedList struct {
 func (ll *LinkedList) PushHead(key string, value int) int {
     n := Node{key, value, ll.head}
 
+    if ll.head == nil {
+        ll.tail = &n
+    }
     ll.head = &n
     ll.length += 1
-    fmt.Println(ll.head)
 
     return ll.length
+}
+
+// Gonna use pointer receiver here to mutate all the stuff
+func (ll *LinkedList) PushBack(key string, value int) int {
+    n := Node{key, value, nil}
+
+    if ll.head == nil {
+        ll.head = &n
+    } else {
+        ll.tail.next = &n
+    }
+    ll.tail = &n
+    ll.length += 1
+
+    return ll.length
+}
+
+func (ll LinkedList) Head() *Node {
+    return ll.head
+}
+
+func (ll LinkedList) Tail() *Node {
+    return ll.tail
 }
 
 // Gonna use value receiver here because i'm just reading
@@ -61,10 +89,18 @@ func main() {
     ll.PushHead("Head1", 3)
     ll.PushHead("Head2", 2)
     ll.PushHead("Head3", 1)
+
+    fmt.Println("Tail:", strconv.Itoa(ll.Tail().value))
+
     ll.PushBack("Back1", 4)
     ll.PushBack("Back2", 5)
     ll.PushBack("Back3", 6)
 
     visit := ll.Visit()
-    fmt.Printf(visit)
+    fmt.Println(visit)
+
+    head := ll.Head()
+    tail := ll.Tail()
+    fmt.Println("Head:", strconv.Itoa(head.value))
+    fmt.Println("Tail:", strconv.Itoa(tail.value))
 }
