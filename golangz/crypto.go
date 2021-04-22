cc46a6928a4b80b787e8c3c20b8b94d785eaea5a
Alain Di Chiappari <alain.devcs@gmail.com>
2021-04-22 06:34:30 +0200

    asm tests

--- golangz/crypto.go
+++ golangz/crypto.go
@@ -1,16 +1,12 @@
 package golangz
 
 import (
-	"fmt"
 	"crypto/sha256"
+	"fmt"
 )
 
 // nil slice
 func TestSha(str string) {
 	sum := sha256.Sum256([]byte(str))
 	fmt.Printf("%x", sum)
-}
-
-func Test() {
-	TestSha("diocan")
 }
---
+++ golangz/toAsm.go
@@ -0,0 +1,7 @@
+package golangz
+
+func Test() int {
+	x := 1
+	y := 2
+	return (x + y)
+}
