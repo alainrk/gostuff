7630f26d345aa0cb25d3f7a3cedf97886cd9eba3
Alain Di Chiappari <alain.devcs@gmail.com>
2021-11-08 15:54:35 +0000

    slices

--- coursera/slices_test.go
+++ coursera/slices_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestSlices1(t *testing.T) {
-	arr := [...]string{"a", "b", "c", "d", "e", "f"}
+	// sli := []string{"a", "b", "c", "d", "e", "f"} // Slice (it hasn't length specified)
+	arr := [...]string{"a", "b", "c", "d", "e", "f"} // Array (it has length specified => implicit with "...")
 
 	s1 := arr[2:4]
 	s2 := arr[3:5]
@@ -13,5 +14,18 @@ func TestSlices1(t *testing.T) {
 	s1[1] = "x"
 	if s2[0] != "x" && arr[3] != "x" {
 		t.Error("should be 'x'")
+	}
+}
+
+func TestSlices2(t *testing.T) {
+	s1 := make([]int, 5)     // length (of the slice) = capacity (length of the underlying array)
+	s2 := make([]int, 5, 10) // length (of the slice) < capacity (length of the underlying array)
+
+	if len(s1) != 5 || cap(s1) != 5 {
+		t.Error("should be 5 and 5")
+	}
+
+	if len(s2) != 5 || cap(s2) != 10 {
+		t.Error("should be 5 and 10")
 	}
 }
