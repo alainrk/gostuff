17846936b292ba934557aa6cff9625c566f84d11
Alain Di Chiappari <alain.devcs@gmail.com>
2021-11-08 15:36:55 +0000

    slices

---
+++ coursera/slices_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSlices1(t *testing.T) {
+	arr := [...]string{"a", "b", "c", "d", "e", "f"}
+
+	s1 := arr[2:4]
+	s2 := arr[3:5]
+
+	s1[1] = "x"
+	if s2[0] != "x" {
+		t.Error("should be 'x'")
+	}
+}
